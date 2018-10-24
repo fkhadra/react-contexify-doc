7e9bf0075de3772d920e5c7740f1153e5ec3d8fd
Fadi Khadra <fdkhadra@gmail.com>
2018-10-24 13:45:40 +0200

    Untested deploy script

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+repository="https://github.com/fkhadra/react-contexify"
+branch="gh-pages"
+
+rm -rf repocache
+
+git clone --single-branch -b $branch $repository repocache
+rm -rf repocache/*
+rsync -avz public/ repocache/
+cd repocache
+
+if [ $? -eq 0 ]
+then
+  git add --all
+  git commit
+  git push origin gh-pages
+  rm -rf repocache
+  exit 0
+fi
+
+exit 1
+
